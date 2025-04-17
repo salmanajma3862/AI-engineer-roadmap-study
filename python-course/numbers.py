2181408680559ed1f730692479179345cf21c639
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-17 04:04:46 -0700

    working with numbers

---
+++ python-course/numbers.py
@@ -0,0 +1,26 @@
+print(2)
+print(2.5)
+print(-2)
+print(2 + 5) # addition
+print(2 - 5) # subtraction
+print(2 * 5) # multiplication
+print(2 / 5) # division
+
+print(2 ** 5) # exponentiation
+
+print (2 + 5 * 4)
+print ((2 + 5) * 5 ) 
+
+# we can store the numbers in variables
+num = 5
+print(num)
+
+# we can convert the numbers into strings
+print(str(num) + " is a number") # this will print the number as a string
+
+# the strings and numbers can be concactenated thats why we convert number into string by add str with the number
+
+print(pow(3, 2)) # this will give us 3 to the power of 2, equals 9
+print(max(10, 6)) # this will give us the maximum number between 10 and 6
+print(min(10, 6)) # this will give us the minimum number between 10 and 6
+print(round(3.2)) # this will round the number to the nearest integer
