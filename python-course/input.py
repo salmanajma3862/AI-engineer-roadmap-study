24e3faddff676db91c0ac4f7b2ed8e0ef55fc5d4
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-17 04:12:19 -0700

    getting the input from user

---
+++ python-course/input.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name: ") # this will take input from the user and store it in the variable name
+age = input ("Enter your age: ") # this will take input from the user and store it in the variable age
+# lets print the name and age using the variables
+print("Hello " + name + ", you are " + age + " years old") # this will print the string "Hello " and then concatenate it with the string stored in the variable name and then concatenate it with the string " years old"
+print("Hello " + name) # this will print the string "Hello " and then concatenate it with the string stored in the variable name
