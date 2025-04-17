8dbd244d09a2a3d42b6a7b3c1360b3713f54d94d
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-17 04:18:48 -0700

    building a basic calculator

---
+++ python-course/calculator.py
@@ -0,0 +1,5 @@
+num1 = input ("Enter first number: ")
+num2 = input ("Enter second number: ")
+
+result = float(num1) + float(num2) # this will convert the string into a float and then add the two numbers together
+print(result) # this will print the result of the addition
