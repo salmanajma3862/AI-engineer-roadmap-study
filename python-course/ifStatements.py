b84f2a62c103c999c2224171ac5875c7167b7114
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-21 06:28:59 -0700

    if statements

---
+++ python-course/ifStatements.py
@@ -0,0 +1,12 @@
+is_male = False
+is_tall = False
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not is_tall:
+    print("You are a male but not tall")
+elif not is_male and is_tall:
+    print("You are not male but tall")
+else:
+    print("You are not male and not tall")
+# This code checks the values of two boolean variables
+
