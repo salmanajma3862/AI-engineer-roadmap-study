810cd836bfd90a2c21bb5d2e2ae4a155b626463d
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-21 06:16:43 -0700

    return statement

---
+++ python-course/return.py
@@ -0,0 +1,10 @@
+def cube(num):
+    """Returns the cube of a number."""
+    return num ** 3
+
+result = cube(4)
+
+print(result)
+
+# This function takes a number as input and returns its cube.
+# The `return` statement is used to send the result back to the caller.
