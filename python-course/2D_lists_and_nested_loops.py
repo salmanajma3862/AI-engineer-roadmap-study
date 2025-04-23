157bc1b4b7c5f3a978dd9703016d04a6ca453137
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-23 07:42:12 -0700

    2d lists

---
+++ python-course/2D_lists_and_nested_loops.py
@@ -0,0 +1,11 @@
+number_grid = [
+    [1, 2, 3], 
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[0][1])  # prints 2
+print(number_grid[2][1])  # prints 8
+
+# this is how we can access the elements of a 2D list
