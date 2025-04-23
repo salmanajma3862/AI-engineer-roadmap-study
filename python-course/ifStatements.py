08d717c80faaab52248ec918e86bbd3208849d7f
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-23 06:14:09 -0700

    if statements

--- python-course/ifStatements.py
+++ python-course/ifStatements.py
@@ -1,12 +1,9 @@
-is_male = False
-is_tall = False
-if is_male and is_tall:
-    print("You are a tall male")
-elif is_male and not is_tall:
-    print("You are a male but not tall")
-elif not is_male and is_tall:
-    print("You are not male but tall")
-else:
-    print("You are not male and not tall")
-# This code checks the values of two boolean variables
-
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+print(max_num(3, 50, 2))  # Output: 50
