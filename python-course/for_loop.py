b333cc5a66ffd4f1b533071a3e6300dacd88cb60
salmanajma3862 <salmanajmal412@gmail.com>
2025-04-23 07:20:00 -0700

    for loop

---
+++ python-course/for_loop.py
@@ -0,0 +1,8 @@
+for letter in 'Python':
+    print(letter)
+friends = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+for friend in friends:
+    print(friend)
+
+for index in range(5):
+    print(index)  # prints numbers from 0 to 4
